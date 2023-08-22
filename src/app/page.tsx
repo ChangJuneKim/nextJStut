2e7031ab6e3db44db2fbfcbe38d4fb87a2e120fc
June <godan6869@naver.com>
2023-08-22 22:47:40 +0900

    feat: 릴리즈 브랜치 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        홈페이지 - 브랜치
+        홈페이지 - 릴리즈 버전
       </div>
       <Counter />
       <Image
